a346e33a276b8d411742c8fadc1aceea81afc5d1
yuktnk <yuki0wl@gmail.com>
2023-04-14 23:17:24 +0900

    useQuery

--- frontend/pages/dashboard.tsx
+++ frontend/pages/dashboard.tsx
@@ -3,11 +3,15 @@ import { useRouter } from 'next/router'
 import axios from 'axios'
 import { LogoutIcon } from '@heroicons/react/solid'
 import { Layout } from '@/components/Layout'
+import { UserInfo } from '@/components/UserInfo'
+import { useQueryClient } from '@tanstack/react-query'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
+  const queryClient = useQueryClient()
 
   const logout = async () => {
+    queryClient.removeQueries(['user']) // ブラウザに保持したキャッシュをremove
     await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`)
     router.push('/')
   }
@@ -18,6 +22,7 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={logout}
       />
+      <UserInfo />
     </Layout>
   )
 }
